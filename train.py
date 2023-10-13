9d2b69e64daded8d016487ef553a82bc9f7ce847
Bardia Ardakanian <bardia.ardakanian@yahoo.com>
2023-10-13 23:03:49 +0330

    fixes

--- train.py
+++ train.py
@@ -80,19 +80,15 @@ def train():
         ])
 
         # Load the dataset
-        # div2k_dataset = DIV2KLoader(div2k_path=args.dataset_root, transform=transform)
-        #
-        # data, data_info = generate_dataset(div2k_dataset=div2k_dataset, num_batches=NUM_BATCHES,
-        #                                             num_images=BATCH_SIZE, image_size=SUB_IMAGE_SIZE)
-        #
-        # div2k_eval_dataset = DIV2KLoader(div2k_path=args.dataset_eval_root, transform=transform)
-        #
-        # eval_data, eval_data_info = generate_dataset(div2k_dataset=div2k_eval_dataset, num_batches=NUM_EVAL_BATCHES,
-        #                                              num_images=BATCH_SIZE, image_size=SUB_IMAGE_SIZE)
-        div2k_dataset = DIV2KLoader(div2k_path=args.dataset_eval_root, transform=transform)
+        div2k_dataset = DIV2KLoader(div2k_path=args.dataset_root, transform=transform)
 
         data, data_info = generate_dataset(div2k_dataset=div2k_dataset, num_batches=NUM_BATCHES,
-                                           num_images=BATCH_SIZE, image_size=SUB_IMAGE_SIZE)
+                                                    num_images=BATCH_SIZE, image_size=SUB_IMAGE_SIZE)
+
+        div2k_eval_dataset = DIV2KLoader(div2k_path=args.dataset_eval_root, transform=transform)
+
+        eval_data, eval_data_info = generate_dataset(div2k_dataset=div2k_eval_dataset, num_batches=NUM_EVAL_BATCHES,
+                                                     num_images=BATCH_SIZE, image_size=SUB_IMAGE_SIZE)
     except:
         raise Exception('Dataset root is invalid')
 
@@ -128,8 +124,8 @@ def train():
     # div2k_eval = DIV2KDataset(eval_data, eval_data_info)
 
     # Initialize the data loader
-    data_loader = DataLoader(div2k_dataset, batch_size=1, shuffle=True)
-    # eval_data_loader = DataLoader(div2k_eval_dataset, batch_size=1, shuffle=True)
+    data_loader = DataLoader(div2k_dataset, batch_size=1, shuffle=False)
+    eval_data_loader = DataLoader(div2k_eval_dataset, batch_size=1, shuffle=False)
     eval_data_loader = data_loader
 
     # Testing dataset performance
